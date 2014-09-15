d803b619468fd7e7d0412ec1394fd9f90ee61734
huimin <hminnnn@gmail.com>
2014-09-16 00:03:25 +0800

    TextBuddyTest
    testDisplay

--- test/textbuddy/TextBuddyTest.java
+++ test/textbuddy/TextBuddyTest.java
@@ -5,12 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TextBuddyTest {
-	
+/*	
 	@Test
-	public void testCommand() {
-		// isAdded
+	public void testIsAdded() {
 		TextBuddy.executeCommand("add this is a brown fox");
 		assertTrue(TextBuddy.isAdded("this is a brown fox"));
+		
+		TextBuddy.executeCommand("addd this cannot be added");
+		assertFalse(TextBuddy.isAdded("addd this cannot be added"));
+	}
+	*/
+	@Test
+	public void testDisplay() {
+		// display
+		TextBuddy.executeCommand("add this is line one");
+		assertEquals("display 1 line.", "1. this is line one", TextBuddy.executeCommand("display"));
 	}
 
 
